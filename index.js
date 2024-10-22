77d61de0bad0cac5ee84b6fab13d33254b38aaaa
Elvis-785 <kiruielvis3358@gmail.com>
2024-10-22 12:30:09 +0300

    concatenation and interpolation in strings.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+//  Write your code in this file!
+const currentUser = 'Elvis Kipkirui';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(null, -9)}!`
+// console.log(shortGreeting)
+
+
